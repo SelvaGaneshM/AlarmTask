9e90a79d02a9ad0331c1a6d6007dfb0fc94f7a80
Selva Ganesh M <selva.ganesh@bofin.com>
2019-05-20 16:13:21 +0530

    home screen working on

--- app/src/main/java/com/selvaganesh/alarmtask/MainActivity.java
+++ app/src/main/java/com/selvaganesh/alarmtask/MainActivity.java
@@ -1,13 +1,34 @@
 package com.selvaganesh.alarmtask;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txt_msg_1)
+    TextView txtMsg1;
+    @BindView(R.id.txt_msg_2)
+    TextView txtMsg2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.create_alarm})
+    public void mainOnClick(View view) {
+        switch (view.getId()) {
+            case R.id.create_alarm:
+                break;
+        }
     }
 }
